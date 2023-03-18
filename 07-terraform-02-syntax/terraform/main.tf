d6a3929d9cea4bc4e047e15d0fc27d101128abab
Timofei Sidnin <sidnintv@gmail.com>
2023-03-18 21:46:32 +0200

    Добавление .gitignore и каталога terraform

---
+++ 07-terraform-02-syntax/terraform/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "vmubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  key_name               = "stv"
+  associate_public_ip_address = true
+  monitoring            = true
+  disable_api_termination = false
+  instance_initiated_shutdown_behavior = "stop"
+
+  vpc_security_group_ids = ["sg-05f3f7b4af3361e8d"]
+  subnet_id              = "subnet-0dcdedcf7497aadb8"
+
+  tags = {
+    Name = "vmubuntu-instance"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
