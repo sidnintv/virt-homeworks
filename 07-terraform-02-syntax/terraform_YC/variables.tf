36ce392309b81e089f69c51338290c1114efeb01
Timofei Sidnin <sidnintv@gmail.com>
2023-03-18 22:29:10 +0200

    Добавление terraform_YC и переименование terraform -> terraform_AWS

---
+++ 07-terraform-02-syntax/terraform_YC/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gvu9hpm8k9vlitbcbq"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gatqjputj5uduabht8"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8mafo8kkgva5brqt57"
+}
