b336636291b4752132f104d51bb4025ccbd4bb25
Timofei Sidnin <sidnintv@gmail.com>
2023-04-16 03:50:47 +0300

    Загружаю файлы

---
+++ 07-terraform-04-teamwork/terraform_module_ec2/outputs.tf
@@ -0,0 +1,19 @@
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_user_id" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "aws_region" {
+  value = data.aws_region.current.name
+}
+
+output "instance_private_ip" {
+  value = aws_instance.vmubuntu.private_ip
+}
+
+output "instance_subnet_id" {
+  value = aws_instance.vmubuntu.subnet_id
+}
