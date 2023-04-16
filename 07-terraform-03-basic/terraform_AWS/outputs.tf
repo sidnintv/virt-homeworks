6853c4521bf66123969d30743acce25071a62ace
Timofei Sidnin <sidnintv@gmail.com>
2023-04-17 02:18:55 +0300

    Update Terraform configuration

--- 07-terraform-03-basic/terraform_AWS/outputs.tf
+++ 07-terraform-03-basic/terraform_AWS/outputs.tf
@@ -25,3 +25,19 @@ output "instance_private_ip" {
 output "instance_subnet_id" {
   value = join(", ", aws_instance.vmubuntu[*].subnet_id)
 }
+
+output "instance_id_foreach" {
+  value = join(", ", values(aws_instance.vmubuntu_foreach)[*].id)
+}
+
+output "instance_public_ip_foreach" {
+  value = join(", ", values(aws_instance.vmubuntu_foreach)[*].public_ip)
+}
+
+output "instance_private_ip_foreach" {
+  value = join(", ", values(aws_instance.vmubuntu_foreach)[*].private_ip)
+}
+
+output "instance_subnet_id_foreach" {
+  value = join(", ", values(aws_instance.vmubuntu_foreach)[*].subnet_id)
+}
