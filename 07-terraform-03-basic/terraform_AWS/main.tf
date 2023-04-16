8dfbf830086ff3482f0197f364dd5bd54f7c62d5
Timofei Sidnin <sidnintv@gmail.com>
2023-04-16 23:43:31 +0300

    Загружаю файлы

--- 07-terraform-03-basic/terraform_AWS/main.tf
+++ 07-terraform-03-basic/terraform_AWS/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "sidninterrastatre"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
